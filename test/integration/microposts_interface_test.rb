899dc560a3e8a78ac701b64cec118d868209ee85
ckashby <clay.ashby@gmail.com>
2018-07-13 13:40:16 -0700

    Added imagemagick to auto-resize uploaded images

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -17,9 +17,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    # picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
